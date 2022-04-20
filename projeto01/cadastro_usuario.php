9a3dcf8fefe3ef85db5f5ac39ea8545f81329b7a
evelin <evelinbonfim57@gmail.com>
2022-04-20 10:13:35 -0300

    projeto001

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,9 @@
+<? 
+    $nome_usuario = $_POST['nome_usuario'];
+    $email_usuario = $_POST['email_usuario'];
+    $fone_usuario = $_POST['fone_usuario'];
+    echo "<p>Nome do usuario: ".$nome_usuario. "<BR>";
+    echo "E-mail: ".$email_usuario. "<BR>";
+    echo "Telefone: ".$fone_usuario. "</p>";
+?>
+
