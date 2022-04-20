432af13f9ee1d5182beb16884bfd8ab2ca723eaa
evelin <evelinbonfim57@gmail.com>
2022-04-20 11:42:07 -0300

    Conexão

--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -6,5 +6,14 @@
     echo "<p>Nome do usuario: ".$nome_usuario. "<BR>";
     echo "E-mail: ".$email_usuario. "<BR>";
     echo "Telefone: ".$fone_usuario. "</p>";
+
+    $sql = "INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+            VALUES ('".$nome. " ', '".$email."', '".$telefone."')";
+            
+    $result = mysqli_query($con, $sql);
+    if($result)
+        echo "Dados inseridos com sucesso";
+    else
+        echo "Erro ao inserir no banco de dados: ".mysqli_error($con);
 ?>
 
